b916ae33044a4eb4fe84f3b70bbdbe9f194b964e
Kiraa Corsac <wire150@gmail.com>
2020-05-01 01:23:00 +0200

    fixed audio

--- GoldenCookieObserver.js
+++ GoldenCookieObserver.js
@@ -9,10 +9,10 @@ class GoldenCookieObserver {
     v.connect(u)
     v.frequency.value = freq
     v.type = "square"
-    u.connect(a.destination)
+    u.connect(this.audio.destination)
     u.gain.value = vol * 0.01
-    v.start(a.currentTime)
-    v.stop(a.currentTime + duration * 0.001)
+    v.start(this.audio.currentTime)
+    v.stop(this.audio.currentTime + duration * 0.001)
   }
 
   observe() {
